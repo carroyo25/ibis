0ee186fa1014b5d29a5096be77e1652b9c2838bb
carroyo25 <73439952+carroyo25@users.noreply.github.com>
2022-11-15 15:30:13 -0500

    1530

--- models/proveedoresmodel.php
+++ models/proveedoresmodel.php
@@ -321,7 +321,7 @@
                                                         INNER JOIN tb_parametros AS bancos ON cm_entidadbco.ncodbco = bancos.nidreg
                                                         INNER JOIN tb_parametros AS monedas ON cm_entidadbco.cmoneda = monedas.nidreg 
                                                     WHERE
-                                                        cm_entidadbco.nitem = :id 
+                                                        cm_entidadbco.id_centi = :id 
                                                         AND cm_entidadbco.nflgactivo = 7");
                 $sql->execute(["id"=>$id]);
                 $rowCount = $sql->rowCount();
