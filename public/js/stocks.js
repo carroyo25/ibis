c1073b99bb3626381adf773b122f0be0e13e2f57
carroyo25 <73439952+carroyo25@users.noreply.github.com>
2023-05-29 19:51:44 -0500

    1951

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -141,7 +141,7 @@ $(() => {
 })
 
 
-detalles = () =>{
+/*detalles = () =>{
     DATA = [];
     let TABLA = $("#tablaPrincipal tbody >tr");
 
@@ -165,6 +165,41 @@ detalles = () =>{
         item['inventario']   = INVENTARIO;
         item['salida']       = SALIDA;
         item['saldo']        = SALDO;
+            
+        DATA.push(item);
+    })
+
+    return DATA;
+}*/
+
+detalles = () =>{
+    DATA = [];
+    let TABLA = $("#tablaPrincipal tbody >tr");
+
+    TABLA.each(function(){
+        let ITEM            = $(this).find('td').eq(0).text(),
+            CODIGO          = $(this).find('td').eq(1).text(),
+            DESCRIPCION     = $(this).find('td').eq(2).text(),
+            UNIDAD          = $(this).find('td').eq(3).text(),
+            INGRESO         = $(this).find('td').eq(4).text(),
+            INVENTARIO      = $(this).find('td').eq(5).text(),
+            SALIDA          = $(this).find('td').eq(6).text(),
+            DEVUELTO        = $(this).find('td').eq(7).text(),
+            TRANSFERENCIA   = $(this).find('td').eq(8).text(),
+            SALDO           = $(this).find('td').eq(10).text();
+           
+        item= {};
+        
+        item['item']            = ITEM;
+        item['codigo']          = CODIGO;
+        item['descripcion']     = DESCRIPCION;
+        item['unidad']          = UNIDAD;
+        item['ingreso']         = INGRESO;
+        item['inventario']      = INVENTARIO;
+        item['salida']          = SALIDA;
+        item['devuelto']        = DEVUELTO;
+        item['transferencias']  = TRANSFERENCIA;
+        item['saldo']           = SALDO;
             
         DATA.push(item);
     })
